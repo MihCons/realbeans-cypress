5e2ea487965a94f74ad46a184cdbd8c416d4189a
Mihai Constantin <r1040189@student.thomasmore.be>
2026-05-23 12:24:45 +0200

    add projectId to config

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,7 @@
 const { defineConfig } = require("cypress");
-
 module.exports = defineConfig({
+  projectId: 'q6hxoe',
   allowCypressEnv: false,
-
   e2e: {
     setupNodeEvents(on, config) {
       // implement node event listeners here
